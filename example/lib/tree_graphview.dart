e366fb32f7e088354f2dddb602cd00c8bd80c6f1
JihwanKim <jihwan.dev@gmail.com>
2022-05-15 21:32:49 +0900

    example update

--- example/lib/tree_graphview.dart
+++ example/lib/tree_graphview.dart
@@ -22,7 +22,8 @@ class _TreeViewPageState extends State<TreeViewPage> {
                   width: 100,
                   child: TextFormField(
                     initialValue: builder.siblingSeparation.toString(),
-                    decoration: InputDecoration(labelText: 'Sibling Separation'),
+                    decoration:
+                        InputDecoration(labelText: 'Sibling Separation'),
                     onChanged: (text) {
                       builder.siblingSeparation = int.tryParse(text) ?? 100;
                       this.setState(() {});
@@ -44,7 +45,8 @@ class _TreeViewPageState extends State<TreeViewPage> {
                   width: 100,
                   child: TextFormField(
                     initialValue: builder.subtreeSeparation.toString(),
-                    decoration: InputDecoration(labelText: 'Subtree separation'),
+                    decoration:
+                        InputDecoration(labelText: 'Subtree separation'),
                     onChanged: (text) {
                       builder.subtreeSeparation = int.tryParse(text) ?? 100;
                       this.setState(() {});
@@ -64,10 +66,8 @@ class _TreeViewPageState extends State<TreeViewPage> {
                 ),
                 ElevatedButton(
                   onPressed: () {
-                    final node12 = Node.Id(r.nextInt(100));
-                    var edge = graph.getNodeAtPosition(r.nextInt(graph.nodeCount()));
-                    print(edge);
-                    graph.addEdge(edge, node12);
+                    final node12 = Node.Id(r.nextInt(10000000));
+                    graph.addNode(node12);
                     setState(() {});
                   },
                   child: Text('Add'),
@@ -82,7 +82,9 @@ class _TreeViewPageState extends State<TreeViewPage> {
                   maxScale: 5.6,
                   child: GraphView(
                     graph: graph,
-                    algorithm: BuchheimWalkerAlgorithm(builder, TreeEdgeRenderer(builder)),
+                    animated: true,
+                    algorithm: BuchheimWalkerAlgorithm(
+                        builder, TreeEdgeRenderer(builder)),
                     paint: Paint()
                       ..color = Colors.green
                       ..strokeWidth = 1
@@ -104,6 +106,12 @@ class _TreeViewPageState extends State<TreeViewPage> {
     return InkWell(
       onTap: () {
         print('clicked');
+
+        final node12 = Node.Id(r.nextInt(10000000));
+        var edge = graph.getNodeUsingId(a!);
+        graph.addEdge(edge, node12);
+
+        this.setState(() {});
       },
       child: Container(
           padding: EdgeInsets.all(16),
